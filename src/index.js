27e4b79ad60648edd0b191772039eab32fe5d382
GaryDiaz <garyking1982@gmail.com>
2022-06-28 14:12:01 -0400

    declarando un ganador

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calcularGanador(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xTurno ? "X" : "O";
     this.setState({ squares: squares, xTurno: !this.state.xTurno });
   }
@@ -30,7 +33,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Siguiente Jugador: " + (this.state.xTurno ? "X" : "O");
+    const ganador = calcularGanador(this.state.squares);
+    let status;
+    if (ganador) {
+      status = "Ganador: " + ganador;
+    } else {
+      status = "Turno de: " + (this.state.xTurno ? "X" : "O");
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -74,3 +83,23 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function calcularGanador(squares) {
+  const lineas = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lineas.length; i++) {
+    const [a, b, c] = lineas[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
